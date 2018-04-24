c8f721757440923fba1791d6cafe30b0d7372dbe
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2018-04-24 21:18:55 +0300

    Add parsing of total size to server

--- build_and_run_logstorageunit.sh
+++ build_and_run_logstorageunit.sh
@@ -2,4 +2,4 @@
 
 ./gradlew storageunit:build
 tar -xf storageunit/build/distributions/storageunit.tar -C storageunit/build/distributions
-storageunit/build/distributions/storageunit/bin/storageunit -p 7777 --page-size 4096 --page-count 4096
+storageunit/build/distributions/storageunit/bin/storageunit -p 7777 --page-size 4096 --total-size 1G
--- storageunit/src/main/java/corfu/storageunit/App.java
+++ storageunit/src/main/java/corfu/storageunit/App.java
@@ -1,5 +1,8 @@
 package corfu.storageunit;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.google.protobuf.InvalidProtocolBufferException;
 import org.apache.commons.cli.*;
 import org.slf4j.Logger;
@@ -31,9 +34,17 @@ public class App {
                         .argName("page count")
                         .type(Number.class)
                         .hasArg(true)
-                        .required()
                         .build();
         options.addOption(pageCountOption);
+
+        final Option totalSizeOption =
+                Option.builder()
+                        .longOpt("total-size")
+                        .argName("total size (e.g., 256M)")
+                        .type(String.class)
+                        .hasArg(true)
+                        .build();
+        options.addOption(totalSizeOption);
 
         final Option portOption =
                 Option.builder("p")
@@ -53,17 +64,82 @@ public class App {
         final CommandLine cmd;
         try {
             cmd = parser.parse(options, args);
-            port = (int) ((long) cmd.getParsedOptionValue("port"));
-            pageSize = (int) ((long) cmd.getParsedOptionValue("page-size"));
-            pageCount = (int) ((long) cmd.getParsedOptionValue("page-count"));
+            port = getPort(cmd);
+            pageSize = getPageSize(cmd);
+            pageCount = getPageCount(cmd, pageSize);
         } catch (ParseException e) {
             logger.error("Error parsing command line", e);
-            System.out.println(e.getMessage());
             formatter.printHelp("<app>", options);
             System.exit(1);
         }
 
         mainWithParams(port, pageSize, pageCount);
+    }
+
+    private static int getPort(final CommandLine cmd) throws ParseException {
+        return (int) ((long) cmd.getParsedOptionValue("port"));
+    }
+
+    private static int getPageSize(final CommandLine cmd) throws ParseException {
+        return (int) ((long) cmd.getParsedOptionValue("page-size"));
+    }
+
+    private static int getPageCount(final CommandLine cmd,
+                                    final int pageSize) throws ParseException {
+        if (cmd.getParsedOptionValue("page-count") != null
+                && cmd.getParsedOptionValue("total-size") != null) {
+            throw new ParseException("Only one of 'page-count' and 'total-size' can be set");
+        }
+
+        if (cmd.getParsedOptionValue("page-count") == null
+                && cmd.getParsedOptionValue("total-size") == null) {
+            throw new ParseException("At least one of 'page-count' and 'total-size' can be set");
+        }
+
+        long result = -1;
+        long totalSize = -1;
+        if (cmd.getParsedOptionValue("page-count") != null) {
+            result = ((long) cmd.getParsedOptionValue("page-count"));
+            totalSize = result * pageSize;
+        } else {
+            final String totalSizeStr = (String) cmd.getParsedOptionValue("total-size");
+            final Matcher m = Pattern.compile("^(\\d*)\\s*([KMG])$").matcher(totalSizeStr);
+            if (!m.matches()) {
+                throw new ParseException("Error parsing " + totalSizeStr);
+            }
+
+            totalSize = Long.parseLong(m.group(1));
+            final String unit = m.group(2);
+            switch (unit) {
+                case "K":
+                    totalSize *= 1024;
+                    break;
+                case "M":
+                    totalSize *= 1024 * 1024;
+                    break;
+                case "G":
+                    totalSize *= 1024 * 1024 * 1024;
+                    break;
+
+                default:
+                    throw new ParseException(String.format("Unsupported unit %s", unit));
+            }
+
+            if (totalSize % pageSize != 0) {
+                throw new ParseException(String.format(
+                        "Total size %s = %d is not a multiple of page size %s",
+                        totalSizeStr, totalSize, pageSize)
+                );
+            }
+
+            result = (totalSize / pageSize);
+        }
+
+        if (totalSize > Integer.MAX_VALUE - 8) {
+            throw new ParseException(String.format("Total size %d is too big", totalSize));
+        }
+
+        return (int) result;
     }
 
     private static void mainWithParams(final int port,
